c9c3ee56063d9ec37c80b3be42103ec9dbc81e0e
Yiming Lin <linyiming.ibug@gmail.com>
2021-08-17 16:44:36 +0100

    update

--- data/download.sh
+++ data/download.sh
@@ -1,1 +1,1 @@
-youtube-dl --download-archive downlaoded.txt -a ori_vids.txt --output original_videos/'%(id)s.%(ext)s'  -i -f 'best[ext=mp4]/best[ext=mkv]'
+youtube-dl --download-archive ./downloaded.txt -a ori_vids.txt --output original_videos/'%(id)s.%(ext)s'  -i -f 'best[ext=mp4]/best[ext=mkv]/best'
